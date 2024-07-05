a21a332218ec2aca644a1b7bd6ebd99d543abc78
Saksham-jain417 <hsr.saksham@gmail.com>
2024-07-05 22:02:24 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -37,5 +37,6 @@ end_time = time.time()
 total_time = round(end_time - start_time, 2)
 
 print("----------------------")
-print("Nice work! You finished in", total_time, "seconds!")
+print("Nice work! You finished in", total_time, "seconds! with:" , wrong , "wrong answers")
+
 
